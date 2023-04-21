8ceba40874f3904e7e706310bb78e995ba0c77e2
Sarah <sarahenya2000@hotmail.dk>
2023-04-21 11:24:34 +0200

    Added write line

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TestGitHub;
+using TestGitHub.ClassSarah;
 
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Keep This");
+Console.WriteLine("Yeppie");
+
+
 
 JulieClass jc = new JulieClass("Julie", "adresse 123");
 Console.WriteLine(jc.Name);
