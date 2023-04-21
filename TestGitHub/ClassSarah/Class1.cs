897628f9ac645b510601a9b70392e1fa76897c05
Sarah <sarahenya2000@hotmail.dk>
2023-04-21 10:58:26 +0200

    Folder Sarah Lavet

---
+++ TestGitHub/ClassSarah/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitHub.ClassSarah
+{
+    public class Class1
+    {
+		private int alder = 25;
+
+		public int Alder
+		{
+			get { return alder; }
+			set { alder = value; }
+		}
+
+		public Class1(int alder)
+		{
+			Alder = alder;
+		}
+	}
+}
