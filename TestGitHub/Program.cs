6981b22ffb1c3ec87afb6d40856cbacb8d2b9b53
Sarah <sarahenya2000@hotmail.dk>
2023-04-21 12:40:56 +0200

    Added more ages

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -6,12 +6,23 @@ Console.WriteLine("This is sprint 1!");
 
 Console.WriteLine("Not This");
 Console.WriteLine("Nope");
+
 Console.WriteLine("TestSarah");
 Console.WriteLine("TestJulie");
 
+
 JulieClass jc = new JulieClass("Julie", "adresse 123");
+Sarah SClass = new Sarah(25);
+
 Console.WriteLine(jc.Name);
 Console.WriteLine(jc.Address);
 
-Sarah SClass = new Sarah(25);
-Console.WriteLine(SClass.Alder);
+Console.WriteLine("Alder:" + SClass.Alder);
+
+Sarah SClass1 = new Sarah(25);
+Sarah SClass2 = new Sarah(19);
+Sarah SClass3 = new Sarah(32);
+
+Console.WriteLine("\t"+SClass.Alder + SClass1.Alder);
+Console.WriteLine("\t"+SClass.Alder + SClass2.Alder);
+Console.WriteLine("\t"+SClass3.Alder + SClass1.Alder);
