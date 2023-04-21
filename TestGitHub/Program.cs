7a14938089fca56a78386a16702062263fd6f11b
juliefroeshoej <julf@itu.dk>
2023-04-21 11:10:49 +0200

    program info Julie

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestGitHub;
+
 Console.WriteLine("Hello, World!");
+
+JulieClass jc = new JulieClass("Julie", "adresse 123");
+Console.WriteLine(jc.Name);
+Console.WriteLine(jc.Address);
